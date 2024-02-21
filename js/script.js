cfc55b3c5fd3c14f8efdf5fb6e580e7b21d773c8
oviyasenthilkumar <154113657+oviyasenthilkumar@users.noreply.github.com>
2024-02-21 09:23:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ let cnt = document.getElementById("cnt");
 let level = document.getElementById("lvl");
 let resultEle = document.getElementById("bmiLvl");
 let data;
-let lose;
+
 
 function calculate(){
     if(age.value ==="" || height.value === "" || weight.value === "" || (male.checked === false && female.checked === false)){
@@ -34,32 +34,8 @@ function calculate(){
             result = "Severe Obese"
         }
 
-        if(bmiCalculate < 16 ){
-            lose =  bmiCalculate - 16;
-        }else if(bmiCalculate >= 16 && bmiCalculate < 17){
-            lose = bmiCalculate - 17;
-        }else if(bmiCalculate >=17 && bmiCalculate < 18.5){
-            lose = bmiCalculate - 18.5;
-        }else if(bmiCalculate >= 18.5 && bmiCalculate < 24.9){
-            lose =  bmiCalculate - 24.9;
-        }else if(bmiCalculate >= 24.9 && bmiCalculate < 29.9){
-            lose =  bmiCalculate - 29.9; 
-        }else if(bmiCalculate >= 30 && bmiCalculate < 34.9){
-            lose =  bmiCalculate - 34.9; 
-        }else if(bmiCalculate > 35){
-            lose = bmiCalculate - 35;
-        }
-        var answer;
-        if(lose<0){
-            answer = `gain your weight by ${Math.abs(lose).toFixed(2)}Kg to get more healthy`
-        }else if(lose == 0){
-            answer = `your are healthy`
-        }else {
-            answer = `lose your Weight by ${lose.toFixed(2)}Kg to get more healthy`
-        }
     data = Math.round(bmiCalculate * 100)/100;
     cnt.innerHTML = `Your BMI = ${data} kg/m2`
-    resultEle.innerHTML =`${answer}` ;
     level.innerHTML = result;
     }
 }
